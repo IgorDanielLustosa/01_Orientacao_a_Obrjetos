220ed87df42d14ddd21d7f9f09a8919044573cac
Igor Daniel A. Lustosa Rosa <105529100+IgorDanielLustosa@users.noreply.github.com>
2024-08-07 21:23:48 -0300

    01. Modelando o ScreenMatch _ Aula finalizada

--- src/Filme.java
+++ src/Filme.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Filme {
+public class Filme {
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    double somaDasAvaliacao;
+    int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+
+    void exibeFichaTecnica(){
+        System.out.println("Nome do Filme: " + nome);
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+    }
+    void avalia(double nota){
+        somaDasAvaliacao += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double pegaMedia(){
+        return somaDasAvaliacao / totalDeAvaliacoes;
+    }
 }
